49dd7933f1ed419e51071e15c0ed2d2f88393e38
vilalali <belaal.ali@gmail.com>
2026-06-14 19:58:40 +0530

    chore: update base path for new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vilal-frontend/',
+  base: '/vilalali/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000
